2b320016f2a5c90dfe94b876f926f4017b40e6e1
LouieBHLu <lmx00518@qq.com>
2023-10-19 20:39:14 -0400

    rebase on main

---
+++ unit_test/client_info_mgt.test.js
@@ -0,0 +1,56 @@
+const {extractKeywords, storeUserData, getUserData, handleUserQuery} = require('../client_info_mgt/client_info_mgt');
+const { OpenAI } = require("langchain/llms/openai");
+
+const model = new OpenAI({})
+
+describe('Test client_info_mgt.js functions', () => {
+
+  test('Test extractKeywords function', async () => {
+    const userPreference = 'tech news';
+    const queryContent = 'latest in tech';
+    const keywords_list = ['sports', 'economic', 'art', 'tech'];
+
+    const keywords = await extractKeywords(
+      queryContent,
+      userPreference,
+      keywords_list,
+      model
+    );
+
+    console.log(keywords);
+  });
+
+  test('Test storeUserData and getUserData functions', async () => {
+    const userId = 'user123';
+    const userPreference = 'tech news';
+    const queryContent = 'latest in tech';
+    const keywords = 'tech'
+
+    // Test storeUserData
+    await storeUserData(userId, userPreference, queryContent, keywords);
+
+    // Test getUserData
+    const user_data = await getUserData(userId);
+
+    expect(user_data.userId).toEqual(userId);
+    expect(user_data.userPreference).toEqual(userPreference);
+    expect(user_data.queryContent).toEqual(queryContent);
+  });
+
+  test('Test handleUserQuery function', async () => {
+    const userId = 'user123';
+    const userPreference = 'tech news';
+    const queryContent = 'latest in tech';
+    const keywords_list = ['sports', 'economic', 'art', 'tech'];
+
+    const keywords = await handleUserQuery(
+      userId,
+      queryContent,
+      userPreference,
+      keywords_list,
+      model
+    );
+
+    console.log(keywords);
+  });
+});
