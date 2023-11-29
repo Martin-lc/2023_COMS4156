42100d623cebc0b4133a35bc3402e928a6a5c231
Martin C. Liu <66339103+Martin-lc@users.noreply.github.com>
2023-11-29 09:34:21 -0500

    Update e2e.test.js
    fix to pass CI test

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -27,6 +27,6 @@ test('submit form and show results', async () => {
     await page.waitForSelector('#results', { visible: true });
 
     const resultsText = await page.$eval('#results', el => el.textContent);
-    expect(resultsText).toContain('Patients with cancer');
+    expect(resultsText).toContain('Patients with COVID');
 }, 30000);
 
