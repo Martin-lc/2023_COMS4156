f60e274bc907384eac10650c10db4d0ff6a993b2
Chen Liu <wilson123121@gmail.com>
2023-11-30 15:22:29 -0500

    fix testing issue regarding to servers; add http-server as a npm package; open up server and app in github action yml file

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -17,9 +17,9 @@ afterAll(async () => {
 });
 
 test('submit form and show results', async () => {
-  await page.goto('http://localhost:3000');
+  await page.goto('http://localhost:8080/client/');
 
-  await page.type('#query', 'I want to learn about long COVID symptoms');
+  await page.type('#query', 'I want to learn about long COVID symptoms as an elder');
   await page.type('#userPreference', 'wellness,elder,covid,nutrition');
   await page.type('#record_num', '2');
   await page.click('button[type="submit"]');
